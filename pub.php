6b2698bb9e0dd06be336819d9cd0d74b4dd0c540
FixnShine <90972263+FixnShine@users.noreply.github.com>
2021-09-21 13:27:10 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -1,8 +1,8 @@
  <?php
  function pubMqtt($topic,$msg){
-       $APPID= "FixitCenter/"; //enter your appid
-     $KEY = "0ZcxhUOlSSrdtMt"; //enter your key
-    $SECRET = "JS9MqcQ2QyuujStrl7Y7Nqim2"; //enter your secret
+       $APPID= "ggwp007bt/"; //enter your appid
+     $KEY = "wvfqNaIvOKa98bI"; //enter your key
+    $SECRET = "bt0rdZvYiVitWb1x57gwCDag9"; //enter your secret
     $Topic = "$topic"; 
       put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
  
